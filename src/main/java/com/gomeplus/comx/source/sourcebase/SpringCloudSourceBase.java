4ba9a9848658598654d45350311d3d6dd5dea112
xue <xuefenglei04@126.com>
2017-02-21 18:21:40 +0800

    t

--- src/main/java/com/gomeplus/comx/source/sourcebase/SpringCloudSourceBase.java
+++ src/main/java/com/gomeplus/comx/source/sourcebase/SpringCloudSourceBase.java
@@ -11,6 +11,7 @@ import com.gomeplus.comx.utils.config.Config;
 import com.gomeplus.comx.utils.config.ConfigException;
 import com.gomeplus.comx.utils.rest.RequestMessage;
 import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -37,24 +38,31 @@ public class SpringCloudSourceBase extends AbstractRequestBasedSourceBase{
     // return restTemplate.getForEntity("http://config-server/user-service-dev.json", Object.class).getBody();
     // 这个时候 request headers 应当已经被处理好保留字段，只需要封装入client
     //TODO timeout 应当于外部已经被设定
+    // rest template 当传递 accept 为 */* 时，返回不能正确解析
     public Object doRequest(RequestMessage requestMessage, Context context) throws SourceException{
         String method                           = requestMessage.getMethod();
         HashMap<String, String> requestheaders  = requestMessage.getHeaderParameters();
         Map<String, Object> data                = requestMessage.getData();
-
         HttpHeaders headers                     = new HttpHeaders();
+
+        requestheaders.remove("Accept");
         headers.setAll(requestheaders);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> entity               = (null == data)? new HttpEntity<String>(headers): new HttpEntity<String>(new JSONObject(data).toString(), headers);
         RestTemplate restTemplate               = context.getRestTemplate();
         URI aURI                                = requestMessage.getUrl().getaURI();
-        ResponseEntity<Object> responseEntity   = restTemplate.exchange(aURI, HttpMethod.resolve(method.toUpperCase()), entity, Object.class);
 
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            return responseEntity.getBody();
-        } else {
-            throw new SourceBizException(responseEntity.getBody().toString(), Integer.parseInt(responseEntity.getStatusCode().toString()));
+        try {
+            ResponseEntity<Object> responseEntity = restTemplate.exchange(aURI, HttpMethod.resolve(method.toUpperCase()), entity, Object.class);
+
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                return responseEntity.getBody();
+            } else {
+                throw new SourceBizException(responseEntity.getBody().toString(), Integer.parseInt(responseEntity.getStatusCode().toString()));
+            }
+        } catch (HttpClientErrorException ex) {
+            throw new SourceBizException(ex.getStatusText(), Integer.parseInt(ex.getStatusCode().toString()));
         }
     }
 }
