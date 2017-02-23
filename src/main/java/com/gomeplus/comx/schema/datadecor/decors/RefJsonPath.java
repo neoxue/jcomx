4f54804f562bec0eb4ea18cbd032784f39149222
xue <xuefenglei04@126.com>
2017-02-23 14:18:32 +0800

    按IDE建议变更

--- src/main/java/com/gomeplus/comx/schema/datadecor/decors/RefJsonPath.java
+++ src/main/java/com/gomeplus/comx/schema/datadecor/decors/RefJsonPath.java
@@ -5,10 +5,7 @@ import com.alibaba.fastjson.JSONPath;
 import com.gomeplus.comx.context.Context;
 import com.gomeplus.comx.utils.config.Config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by xue on 2/2/17.
@@ -21,20 +18,20 @@ public interface RefJsonPath {
     default List<Object> getAllNodes(Config conf, Object data, Context context) {
         String refJsonPath = conf.str(FIELD_REF_JSON_PATH, "");
         if (refJsonPath.isEmpty()) {
-            context.getLogger().debug("RefJsonPath: key refJsonPath empty");
-            return Arrays.asList(data);
+            context.getLogger().trace("RefJsonPath: key refJsonPath empty");
+            return Collections.singletonList(data);
+            //return Arrays.asList(data);
         }
         try {
             Object matchedNode = JSONPath.eval(data, refJsonPath);
             if (matchedNode instanceof List) {
-                //context.getLogger().debug("RefJsonPath: List:" + matchedNode.toString());
                 return (List) matchedNode;
             } else if(matchedNode instanceof Collection) {
-                //context.getLogger().debug("RefJsonPath: Collection:" + matchedNode.toString());
                 return Arrays.asList(((Collection) matchedNode).toArray());
             }
-            context.getLogger().error("RefJsonPath: not list or collection:" + matchedNode.toString() + " class:" + matchedNode.getClass() + " refjsonpath:" + refJsonPath);
-            return Arrays.asList(matchedNode);
+            context.getLogger().warn("RefJsonPath: not list or collection:" + matchedNode.toString() + " class:" + matchedNode.getClass() + " refjsonpath:" + refJsonPath);
+            return Collections.singletonList(matchedNode);
+            //return Arrays.asList(matchedNode);
         } catch(Exception ex){
             context.getLogger().error("Decor RefJsonPath, refJsonPath error:" + ex.getMessage() + " refJsonPath:"+ refJsonPath+ ", data:" + data);
             return new ArrayList<>();
@@ -42,13 +39,12 @@ public interface RefJsonPath {
     }
 
     default List<Object> getMatchedNodes(Config conf, Object data, Context context) {
-        List<Object> allNodes = getAllNodes(conf, data, context);
-        context.getLogger().debug("RefJsonPath: got Nodes(all):" + allNodes.toString());
+        List<Object> allNodes   = getAllNodes(conf, data, context);
+        context.getLogger().trace("RefJsonPath: got Nodes(all):" + allNodes.toString());
 
-        String precondition = conf.str(FIELD_REF_PRECONDITION, "");
-        if (precondition.isEmpty()) {
-            return allNodes;
-        }
+        String precondition     = conf.str(FIELD_REF_PRECONDITION, "");
+        if (precondition.isEmpty())  return allNodes;
+
         List<Object> matchedNodes = new ArrayList<Object>();
         for (Object node:allNodes) {
             if (Precondition.execute(precondition, data, context, node)) matchedNodes.add(node);
